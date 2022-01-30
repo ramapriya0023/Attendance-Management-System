7751f86c306f0c7cd94b2e66ec1f5947c8646d86
Vijayaraghavan D <59435339+Vijayaraghavan-VR@users.noreply.github.com>
2022-01-31 03:21:09 +0530

    Resolved Index

--- index.py
+++ index.py
@@ -5,6 +5,5 @@ from routes.USER_ROUTE import router
 from routes.WorkHoursRoute import rou
 app = FastAPI()
 app.include_router(leaveroute)
-#app.include_router(user)
 app.include_router(router)
 app.include_router(rou)
